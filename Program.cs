c71a29e31de27c4e149a291ca5b630682248bea5
the-bug-lover <aleksander24sfj@gmail.com>
2024-11-25 15:00:25 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System.Reflection.Metadata.Ecma335;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseDefaultFiles();
+
+app.MapGet("/health", () => "Server OK");
+
+app.Run();
